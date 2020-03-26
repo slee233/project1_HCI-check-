931311ef499e927b551b80bba5e06db5161edee8
slee233 <59944528+slee233@users.noreply.github.com>
2020-03-25 20:07:42 -0500

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,26 @@
+function a(){
+	confirm("Do you want to Sign Up?");
+}
+function b(){
+	alert("Your Sign up has completed successfully");
+	//window.history.back();	
+}
+
+function validate(){
+	if(f.id.value == ""){
+		alert("please Enter your ID");	
+		return false;
+	}
+	if(f.pw.value == ""){
+		alert("please Enter your PW");
+		return false;
+	}
+	if(f.email.value == ""){
+		alert("please Enter your Email");
+		return false;
+	}
+	if(f.phone.value == ""){
+		alert("please Enter your Phone");
+		return false;
+	}		
+}
